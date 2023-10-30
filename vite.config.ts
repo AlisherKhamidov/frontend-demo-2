eac2bdb4a9690db32c7e5d537f4232662a5f5931
Alisher Khamidov <110631572+AlisherKhamidov@users.noreply.github.com>
2023-10-30 20:55:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,17 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
 						target: 'https://squid-app-trpsn.ondigitalocean.app',
 						changeOrigin: true,
 						agent: new http.Agent(),
+						ws: true,
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url);
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+            });
 					},
 				},
 			},
