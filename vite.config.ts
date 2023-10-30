975ac4f34d4b46b0cb2e42f2afc10f7109d47e09
Alisher Khamidov <110631572+AlisherKhamidov@users.noreply.github.com>
2023-10-30 20:34:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import http from 'https';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (mode === 'development') {
-    return {
+	if (mode === 'development') {
+		return {
 			plugins: [react()],
 			server: {
 				open: true,
@@ -24,17 +25,18 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
 				setupFiles: 'src/setupTests',
 				mockReset: true,
 			},
-		
-    }
-  } else {
-    // command === 'build'
-    return {
+		};
+	} else {
+		// command === 'build'
+		return {
 			plugins: [react()],
 			server: {
 				open: true,
 				proxy: {
 					'/api': {
 						target: 'https://squid-app-trpsn.ondigitalocean.app/',
+						changeOrigin: true,
+						agent: new http.Agent(),
 					},
 				},
 			},
@@ -49,7 +51,6 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
 				setupFiles: 'src/setupTests',
 				mockReset: true,
 			},
-		
-    }
-  }
-})
+		};
+	}
+});
